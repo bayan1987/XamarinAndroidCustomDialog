a18bdf4d914d5f825a9eecb8ae3e774926ac8b78
Val Okafor <okaatech@yahoo.com>
2015-01-24 15:57:38 -0800

    Add Dialog Layout

--- XamarinDroidCustomListView/Resources/Resource.Designer.cs
+++ XamarinDroidCustomListView/Resources/Resource.Designer.cs
@@ -41,6 +41,22 @@ namespace XamarinDroidCustomListView
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int gray = 2130968576;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -63,29 +79,62 @@ namespace XamarinDroidCustomListView
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int buttonDeleteService = 2131034117;
+			// aapt resource value: 0x7f070010
+			public const int buttonDeleteService = 2131165200;
 			
-			// aapt resource value: 0x7f050004
-			public const int buttonEditService = 2131034116;
+			// aapt resource value: 0x7f07000f
+			public const int buttonEditService = 2131165199;
 			
-			// aapt resource value: 0x7f050001
-			public const int linearLayout1 = 2131034113;
+			// aapt resource value: 0x7f07000b
+			public const int checkBox1 = 2131165195;
 			
-			// aapt resource value: 0x7f050006
-			public const int service_list = 2131034118;
+			// aapt resource value: 0x7f070006
+			public const int editTextDescription = 2131165190;
 			
-			// aapt resource value: 0x7f050007
-			public const int service_list_empty = 2131034119;
+			// aapt resource value: 0x7f070004
+			public const int editTextName = 2131165188;
 			
-			// aapt resource value: 0x7f050002
-			public const int tvServiceCategory = 2131034114;
+			// aapt resource value: 0x7f070008
+			public const int editTextPrice = 2131165192;
 			
-			// aapt resource value: 0x7f050000
-			public const int tvServiceName = 2131034112;
+			// aapt resource value: 0x7f070009
+			public const int linearLayout1 = 2131165193;
 			
-			// aapt resource value: 0x7f050003
-			public const int tvServicePrice = 2131034115;
+			// aapt resource value: 0x7f070001
+			public const int linearLayoutContainer = 2131165185;
+			
+			// aapt resource value: 0x7f070000
+			public const int scrollView1 = 2131165184;
+			
+			// aapt resource value: 0x7f070011
+			public const int service_list = 2131165201;
+			
+			// aapt resource value: 0x7f070012
+			public const int service_list_empty = 2131165202;
+			
+			// aapt resource value: 0x7f07000a
+			public const int spinnerCategory = 2131165194;
+			
+			// aapt resource value: 0x7f070002
+			public const int textTitle = 2131165186;
+			
+			// aapt resource value: 0x7f070005
+			public const int textViewDescription = 2131165189;
+			
+			// aapt resource value: 0x7f070003
+			public const int textViewName = 2131165187;
+			
+			// aapt resource value: 0x7f070007
+			public const int textViewPrice = 2131165191;
+			
+			// aapt resource value: 0x7f07000d
+			public const int tvServiceCategory = 2131165197;
+			
+			// aapt resource value: 0x7f07000c
+			public const int tvServiceName = 2131165196;
+			
+			// aapt resource value: 0x7f07000e
+			public const int tvServicePrice = 2131165198;
 			
 			static Id()
 			{
@@ -122,14 +171,17 @@ namespace XamarinDroidCustomListView
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
-			// aapt resource value: 0x7f040002
-			public const int no_services = 2130968578;
+			// aapt resource value: 0x7f050003
+			public const int add_service_item = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int no_services = 2131034114;
 			
 			static String()
 			{
@@ -137,6 +189,25 @@ namespace XamarinDroidCustomListView
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int DividerStyle = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int LabelStyle = 2131099649;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
